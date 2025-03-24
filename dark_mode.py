49b241d21688ca91812e18db4a21d75540a68dbf
Quantum Nomad <177103775+byteephantom@users.noreply.github.com>
2025-03-24 14:05:28 +0530

    dark_mode_feature_for_understanding.py

--- dark_mode.py
+++ dark_mode.py
@@ -1,13 +1,3 @@
-# from nicegui import ui
-
-# dark = ui.dark_mode()
-# ui.label('Switch mode:')
-# ui.button('Dark', on_click=dark.enable)
-# ui.button('Light', on_click=dark.disable)
-
-# ui.run()
-# Importing Tkinter library  
-# in the environment 
 from tkinter import *
   
 # Creating a window 
